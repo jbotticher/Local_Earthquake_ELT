0d52f0f743e91c57ced16fe4c2379a18ab01ba1c
joshua botticher <joshuabotticher@joshuas-MacBook-Air.local>
2024-08-18 12:06:17 -0400

    Remove app/airbyte and misc directories from repository

--- misc/dagster_elt copy/dagster_elt/schedules/__init__.py
+++
@@ -1,4 +0,0 @@
-from dagster import ScheduleDefinition
-from dagster_elt.jobs import earthquake_pipeline
-
-earthquake_pipeline_schedule = ScheduleDefinition(job=earthquake_pipeline, cron_schedule="*/5 * * * *")
--- misc/old_stuff/dagster_repo.py
+++
@@ -1,9 +0,0 @@
-from dagster import repository
-from earthquake_elt.App.project.pipeline.dagster_pipeline_inc import earthquake_pipeline, schedule
-
-@repository
-def my_repository():
-    return [
-        earthquake_pipeline,
-        schedule
-    ]
